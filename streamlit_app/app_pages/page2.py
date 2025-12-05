46d1015e8efc86cdeffc801047e6c7bd9f89401a
tb9757 <tb9757@gmail.com>
2025-12-05 14:07:35 +0000

    Add charts

--- streamlit_app/app_pages/page2.py
+++ streamlit_app/app_pages/page2.py
@@ -1,6 +1,15 @@
 import streamlit as st
-
+import pandas as pd
+import plotly.express as px
 def page2_body():
     """
     This function displays the content of Page one.
     """
+    df = pd.read_csv('../data/penguins_cleaned.csv')
+    st.scatter_chart(data=df, x='bill_length_mm', y='bill_depth_mm', x_label='Bill Length (mm)', y_label='bill depth (mm)', color='species')
+    st.scatter_chart(data=df, x='body_mass_g', y='bill_depth_mm', x_label='Body mass (G)', y_label='bill depth (mm)', color='diet')
+                     
+
+    # fig = px.scatter(df, x='bill_length_mm', y='bill_depth_mm', color='species', size='body_mass_g', hover_data=['island', 'sex'])
+    # title='Penguin Species Clustering by Bill Dimensions'
+    # fig.show()
