76a93697020cf91c0b5645d0b1d822871252af88
tb9757 <tb9757@gmail.com>
2025-12-05 10:30:11 +0000

    Add streamlit title

---
+++ streamlit/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#print(st.__version__)
+
+st.title("Palmers Penguins ML Classification Example")
+# st.header("This is a header")
+# st.subheader("This is a subheader")
+# st.text("This is fixed-width text.")
+# st.markdown("**This text is bold using Markdown!**")
