31e70504f478b589faf6a0fae036420e6886b921
tb9757 <tb9757@gmail.com>
2025-12-05 10:57:57 +0000

    Add button to show dataframe

--- streamlit/app.py
+++ streamlit/app.py
@@ -4,12 +4,12 @@ import numpy as np
 
 #print(st.__version__)
 
-st.title("Palmers Penguins ML Classification Example")
+st.title("Palmers Penguins ML Classification")
 # st.header("This is a header")
 # st.subheader("This is a subheader")
 # st.text("This is fixed-width text.")
 # st.markdown("**This text is bold using Markdown!**")
 
 df = pd.read_csv('../data/penguins_cleaned.csv')
-
-st.dataframe(df)
+if st.button('View Data'):
+    st.dataframe(df.head())
