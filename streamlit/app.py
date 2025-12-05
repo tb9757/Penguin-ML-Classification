e06f85be285a77156e746dfb56ef3af6143480f9
tb9757 <tb9757@gmail.com>
2025-12-05 10:34:53 +0000

    Add dataframe to streamlit app

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 #print(st.__version__)
 
@@ -7,3 +9,7 @@ st.title("Palmers Penguins ML Classification Example")
 # st.subheader("This is a subheader")
 # st.text("This is fixed-width text.")
 # st.markdown("**This text is bold using Markdown!**")
+
+df = pd.read_csv('../data/penguins_cleaned.csv')
+
+st.dataframe(df)
